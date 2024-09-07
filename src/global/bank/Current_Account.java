b934e6b7d54df77d2382a77f8ab1623ced0a321d
MAYUR RATHORE <hackercodder@gmail.com>
2024-09-07 11:30:05 +0530

    project update

---
+++ src/global/bank/Current_Account.java
@@ -0,0 +1,13 @@
+class Current_Account extends Account{
+
+
+private final int  Interest_Rate ;
+private final String  Date_of_Opening ;
+private int Ammount ;
+private String  type ;
+Public void addCustomer(){ 
+}
+Public void removeCustomer(){ 
+
+
+}
---
+++ src/global/bank/Customer.java
@@ -0,0 +1,14 @@
+interface Customer{
+
+
+private final int  Cust_ID ;
+private int Name ;
+private String  Address ;
+private int Phone ;
+
+
+public void Request_Loan(){
+
+}
+
+}
