8e46cf3dcf8d7f94a1ccf033ad2871798253571c
MAYUR RATHORE <hackercodder@gmail.com>
2024-09-07 12:17:26 +0530

    project

---
+++ src/global/bank/Bank.java
@@ -0,0 +1,27 @@
+ Public class Bank{
+
+static  String Bank_name ="Global Bank";
+private String Name ;
+private  Name ;
+
+Public void addBranch(){
+
+
+}
+Public void removeBranch(){
+
+    
+}
+Public void getBranch(){
+
+    
+}
+Public void getAllBranches(){
+
+    
+}
+
+
+
+
+}
